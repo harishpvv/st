f9ec618df9616de8195aaa6b7b57515bf1e400f4
Harish PVV <52023936+harishpvv@users.noreply.github.com>
2024-05-07 15:37:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,12 @@ Hello everyone
 """)
 
 st.latex(r''' e^{i\pi} + 1 = 0 ''')
+
+
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+tab1.write("this is tab 1")
+tab2.write("this is tab 2")
+
+# You can also use "with" notation:
+with tab1:
+  st.radio('Select one:', [1, 2])
