cb87dcb261537bffa1042efc5815706d6a2a1cc4
Harish PVV <52023936+harishpvv@users.noreply.github.com>
2024-05-07 15:38:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,4 @@ tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
 tab1.write("this is tab 1")
 tab2.write("this is tab 2")
 
-# You can also use "with" notation:
-with tab1:
-  st.radio('Select one:', [1, 2])
-
-col1, col2 = st.columns(2)
-col1.write('Column 1')
-col2.write('Column 2')
-
-# Three columns with different widths
-col1, col2, col3 = st.columns([3,1,1])
-# col1 is wider
 
